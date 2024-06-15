38a23a06adc7850be52f1ab69c57f33f1625f39f
Pedro Henrique Pereira Carvalho <124099730+PedroHPCarvalho@users.noreply.github.com>
2024-06-15 17:01:48 -0300

    Add files via upload

---
+++ LeitorDePensamento/src/classes/TelaGenio.java
@@ -0,0 +1,130 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package classes;
+
+import java.awt.Font;
+
+/**
+ *
+ * @author pedro
+ */
+public class TelaGenio extends javax.swing.JFrame {
+
+    /**
+     * Creates new form TelaGenio
+     */
+    public TelaGenio() {
+        initComponents();
+        lblPalpite.setText("<html>Vou pensar em um valor de <strong> 1 a 10 </strong>. tente adivinhar</html>");
+    }
+
+    /**
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lblPalpite = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txtVal = new javax.swing.JSpinner();
+        btnPalpite = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        lblPalpite.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblPalpite.setText("Frase");
+        getContentPane().add(lblPalpite, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 80, 190, 80));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/balao.png"))); // NOI18N
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 30, -1, -1));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/genio.jpeg"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 220, -1, -1));
+
+        jLabel4.setText("Valor");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, -1, -1));
+
+        txtVal.setModel(new javax.swing.SpinnerNumberModel(1, 1, 10, 1));
+        getContentPane().add(txtVal, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 290, 90, -1));
+
+        btnPalpite.setText("Palpite");
+        btnPalpite.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPalpiteActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnPalpite, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 330, 100, 50));
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPalpiteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPalpiteActionPerformed
+        // TODO add your handling code here:
+      double n = 1+ Math.random()* (11-1);
+      int valor =  (int) n;
+      
+      int num = Integer.parseInt(txtVal.getValue().toString());
+      
+      String f1 = "<html>ACERTOU!</html>";
+      String f2 = "<html>ERROU! Eu pensei no numero "+valor+"</html>";
+      
+      String res = (valor == num )?f1:f2;
+      
+      lblPalpite.setFont(new Font("Arial Black",Font.PLAIN,16));
+      
+      lblPalpite.setText(res);
+      
+    }//GEN-LAST:event_btnPalpiteActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(TelaGenio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(TelaGenio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(TelaGenio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TelaGenio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new TelaGenio().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnPalpite;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel lblPalpite;
+    private javax.swing.JSpinner txtVal;
+    // End of variables declaration//GEN-END:variables
+
+
+}
